4e47bee30efe9aa6f01f026df30226541ce1cd1b
Anngelyque <anngelyque@gmail.com>
2020-07-20 12:57:59 -0700

    Create play_through.js

---
+++ play_through.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  skip through the lectures, pause at the quizzes
+// @author       Anngelyque
+// @match        https://*.gatech.instructure.com/courses*pages*
+// @grant        none
+// @run-at document-end
+// ==/UserScript==
+
+window.addEventListener("load", function()
+{
+    var delayInMiliseconds = 1000;
+    var button = document.getElementsByClassName("Button")[3];
+
+    setInterval(function(){
+        button.click();
+    }, delayInMiliseconds);
+});
