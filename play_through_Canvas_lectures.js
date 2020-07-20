25d464eb66ea74cd919c7699c555b996a5af757b
Anngelyque <anngelyque@gmail.com>
2020-07-20 13:13:52 -0700

    Update play_through_Canvas_lectures.js

--- play_through_Canvas_lectures.js
+++ play_through_Canvas_lectures.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         PlayThroughCanvasLectures
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  skip through the lectures, pause at the quizzes
